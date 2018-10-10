e82611d2faffeb51e548c139715192f7517f9fc4
Michelle S <MichelleShaheen@cmail.carleton.ca>
2018-10-10 02:20:38 -0600

    init: test for pair

--- src/main/java/core/Hand.java
+++ src/main/java/core/Hand.java
@@ -5,16 +5,23 @@ import java.util.ArrayList;
 public class Hand {
 	
 	private ArrayList<Card> cards;
+	private String temp;
 	
-	public Hand(){
+	public Hand(String myHand){
 		cards = new ArrayList<Card>();
+		String[] myCards = myHand.split(" ");
+		this.temp = myCards[0];
+		for(int i = 1; i < 6; i++){
+			cards.add(new Card(myCards[i]));
+		}
 	}
 	
 	public ArrayList<Card> getCard(){
 		return cards;
 	}
 	
-	public boolean addHand(Card cd){
+
+	public boolean addCard(Card cd){
 		cards.add(cd);
 		if(isFull())
 			return true;
@@ -28,5 +35,9 @@ public class Hand {
 		}
 		// valid ID?
 		return true; 
+	}
+	
+	public boolean fullHouse(){
+		return true;
 	}
 }
--- src/test/java/core/TestHand.java
+++ src/test/java/core/TestHand.java
@@ -6,20 +6,28 @@ public class TestHand extends TestCase {
 	
 	public void testNoCards() {
   
-		Hand h = new Hand();
+		Hand h = new Hand("");
 		assertEquals(false, h.isFull());
 	}
 	
-	public void testAddHand() {
+	public void testAddCard() {
 		  
-		Hand h = new Hand();
+		Hand h = new Hand("AS");
 		Card c = new Card("AS");
-		assertTrue(h.addHand(c));
+		assertTrue(h.addCard(c));
 	}
 	
+	public void testPair(){
+		Hand p = new Hand("3D 3H 2C 6S 8D");
+		assertTrue(p.pair());
+	}
+	
+	
+	// fullhouse contains a pair and triple
+	// finish method later when pair and triple established
 	public void testFullHouse(){
-		Hand fullHouse = new Hand("3D 3H 3S 2H 2S");
-		assertTrue(fullHouse.isFullHouse());
+		Hand fh = new Hand("3D 3H 3S 2H 2S");
+		assertTrue(fh.fullHouse());
 	}
 	
 }
