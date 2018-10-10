dcde7124fefaddae2dc53d82470a6eebb0c97a91
Michelle S <MichelleShaheen@cmail.carleton.ca>
2018-10-09 19:11:17 -0600

    init: create suit test

--- src/test/java/core/TestCard.java
+++ src/test/java/core/TestCard.java
@@ -8,10 +8,15 @@ import junit.framework.TestCase;
 public class TestCard extends TestCase {
 	
 	
-	public void testNewCardName() {
-		Card c = new Card("AS");
-		
-		assertEquals("AS", c.getName());
+	public void testName() {
+		Card c = new Card("3D");
+		assertEquals("3D", c.getName());
+	}
+	
+	public void testSuit() {
+		Card c = new Card();
+		Suit s = c.Suit("Spades");
+		assertEquals(s, Suit.SPADES);
 	}
 	
 }
