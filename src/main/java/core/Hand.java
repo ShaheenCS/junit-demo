e05d79f70e692a7e0f222e241ddaed439ac4ed98
Michelle S <MichelleShaheen@cmail.carleton.ca>
2018-10-10 03:44:27 -0600

    refactor: reverse the damage

--- src/main/java/core/Hand.java
+++ src/main/java/core/Hand.java
@@ -1,27 +1,20 @@
 package core;
 
-import java.util.ArrayList;
-
-public class Hand {
+ import java.util.ArrayList;
+ 
+ public class Hand {
 	
 	private ArrayList<Card> cards;
-	private String temp;
 	
-	public Hand(String myHand){
+	public Hand(){
 		cards = new ArrayList<Card>();
-		String[] myCards = myHand.split(" ");
-		this.temp = myCards[0];
-		for(int i = 1; i < 6; i++){
-			cards.add(new Card(myCards[i]));
-		}
 	}
 	
 	public ArrayList<Card> getCard(){
 		return cards;
 	}
 	
-
-	public boolean addCard(Card cd){
+	public boolean addHand(Card cd){
 		cards.add(cd);
 		if(isFull())
 			return true;
@@ -34,10 +27,6 @@ public class Hand {
 			return false;
 		}
 		// valid ID?
-		return true; 
-	}
-	
-	public boolean fullHouse(){
 		return true;
-	}
+		}
 }
--- src/test/java/core/TestHand.java
+++ src/test/java/core/TestHand.java
@@ -6,29 +6,15 @@ public class TestHand extends TestCase {
 	
 	public void testNoCards() {
   
-		Hand h = new Hand("");
+		Hand h = new Hand();
 		assertEquals(false, h.isFull());
 	}
 	
 	public void testAddCard() {
 		  
-		Hand h = new Hand("AS");
+		Hand h = new Hand();
 		Card c = new Card("AS");
-		assertTrue(h.addCard(c));
+		assertTrue(h.addHand(c));
 	}
-	
-	public void testPair(){
-		Hand p = new Hand("3D 3H 2C 6S 8D");
-		assertTrue(p.pair());
-	}
-	
-	
-	// fullhouse contains a pair and triple
-	// finish method later when pair and triple established
-	public void testFullHouse(){
-		Hand fh = new Hand("3D 3H 3S 2H 2S");
-		assertTrue(fh.fullHouse());
-	}
-	
 }
 
