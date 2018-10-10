31be1dc2fe7066ad3ba1b37faabfab03d2baf421
Michelle S <MichelleShaheen@cmail.carleton.ca>
2018-10-09 20:48:04 -0600

    init: added method to test deck size is 52

--- src/test/java/core/TestDeck.java
+++ src/test/java/core/TestDeck.java
@@ -3,5 +3,9 @@ package core;
 import junit.framework.TestCase;
 
 public class TestDeck extends TestCase {
-
+	//test for 52 cards
+	public void testFullDeck(){
+		Deck deck = new Deck();
+		assertEquals(52, deck.cards.size());
+	}
 }
