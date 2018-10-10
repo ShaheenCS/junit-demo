8fb2003befb6dbcfef7d95e787eb7daa184992b6
Michelle S <MichelleShaheen@cmail.carleton.ca>
2018-10-09 23:54:17 -0600

    init: test empty hand method added

--- src/test/java/core/TestHand.java
+++ src/test/java/core/TestHand.java
@@ -1,5 +1,16 @@
 package core;
 
-public class TestHand {
+import junit.framework.TestCase;
 
+public class TestHand extends TestCase {
+	
+	public void testNoCards() {
+		setUp();
+		String str = "";
+		// we want hand to add hand to player  
+		boolean b = r.hand(str);
+		assertEquals(b, false);
+	}
+	
 }
+
