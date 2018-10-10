002731c2825159e5b757fd46cca2c9ad7e2dfa0f
Michelle S <MichelleShaheen@cmail.carleton.ca>
2018-10-09 19:33:47 -0600

    init: added rank method

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -12,7 +12,8 @@ public class Card implements Comparable<Card>{
 	public String getName() {
 		return name;
 	}
-		
+	
+	// could be enum but switch keeps in same class -- cleaner
 	public Suit makeSuit(String str) {
 		switch(str) {
 		case "Spades":
@@ -23,6 +24,40 @@ public class Card implements Comparable<Card>{
 			return Suit.HEARTS;
 		case "Diamonds":
 			return Suit.DIAMONDS;
+		default:
+			return null;
+		}
+	}
+	
+	// could use enum in separate file but this more clean
+	public Rank makeRank(String str) {
+		switch(str) {
+		case "Two":
+			return Rank.TWO;
+		case "Three":
+			return Rank.THREE;
+		case "Four":
+			return Rank.FOUR;
+		case "Five":
+			return Rank.FIVE;
+		case "Six":
+			return Rank.SIX;
+		case "Seven":
+			return Rank.SEVEN;
+		case "Eight":
+			return Rank.EIGHT;
+		case "Nine":
+			return Rank.NINE;
+		case "Ten":
+			return Rank.TEN;
+		case "Jack":
+			return Rank.JACK;
+		case "Queen":
+			return Rank.QUEEN;
+		case "King":
+			return Rank.KING;
+		case "Ace":
+			return Rank.ACE;
 		default:
 			return null;
 		}
